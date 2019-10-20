95a755213996f047320884c8088cefa469c95933
Ilya Avdeev <avdeeff.ilya@gmail.com>
2019-10-20 21:49:11 +0300

    Get rid of ugly Trial button on the main toolbar

--- soapui/src/main/java/com/eviware/soapui/SoapUI.java
+++ soapui/src/main/java/com/eviware/soapui/SoapUI.java
@@ -361,9 +361,6 @@ public class SoapUI {
         mainToolbar.add(new NewProjectActionDelegate("/new-rest-project-icon.png", "REST", NewRestProjectAction.SOAPUI_ACTION_ID));
         mainToolbar.add(new ImportWsdlProjectActionDelegate());
         mainToolbar.add(new SaveAllActionDelegate());
-        mainToolbar.addSpace(2);
-        mainToolbar.add(new ShowOnlineHelpAction("Trial", HelpUrls.TRIAL_URL, "Apply for SoapUI Pro Trial License",
-                "/Trial_20-20.png"));
         mainToolbar.add(new PreferencesActionDelegate());
         applyProxyButton = (JToggleButton) mainToolbar.add(new JToggleButton(new ApplyProxyButtonAction()));
         updateProxyButtonAndTooltip();
